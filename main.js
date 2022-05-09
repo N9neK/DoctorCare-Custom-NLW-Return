4beeaad24ad6a7ee50312542e4acda53bd9a5c80
N9neK <ll.nine.kouzuki.ll@gmail.com>
2022-05-08 23:44:18 -0300

    Dia 5 e projeto finalizado. JS feito.

--- main.js
+++ main.js
@@ -10,6 +10,39 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  const sectionTop = section.offsetTop //topo da seção
+  const sectionHeight = section.offsetHeight // altura da seção
+  // O topo da seção chegou/passou a linha alvo
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+  // onde a seção termina
+  const sectionEndsAt = sectionTop + sectionHeight
+  // final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 //Mostrar o Nav ao fazer o Scroll
